61ac8d29b55cee89a49956685d8f152dd70c50db
Islam Turky <107361423+Islam-Turky@users.noreply.github.com>
2023-07-04 14:29:43 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const myCirtifications = [
+    {
+        id: 'one',
+        title:'Web development Challenger level',
+        images: '/U.png',
+        width: '480px',
+        height: '300px',
+    },
+    {
+        id: 'two',
+        title:'Introduction To AI',
+        images: '/AI.png',
+        width: '550px',
+        height: '400px',
+    },
+    {
+        id: 'three',
+        title: 'Soft Skills',
+        images: '/m.png',
+        width: '550px',
+        height: '400px',
+    },
+    {
+        id: 'four',
+        title: 'Hackathon Benha',
+        images: '/hackathon.jpg',
+        width: '575px',
+        height: '400px',
+    }
+];
