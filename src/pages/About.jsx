9cc20ac57a24c3b1a1d43f32b4c467f848cc309a
Islam-Turky <iturky06@gmail.com>
2024-01-28 15:11:02 +0200

    about section 2.1.0

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import Footer from '../components/Footer'
-import { FaReact, FaJs, FaPython, FaNodeJs} from "react-icons/fa6";
+// import { FaReact, FaJs, FaPython, FaNodeJs} from "react-icons/fa6";
 import { myCirtifications } from '../data';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -19,69 +19,39 @@ const About = () => {
         {/* <AboutNavbar /> */}
         <section id='about'>
             {/* cv */}
-            <div id='cv' className='secti'>
-                <div>
-                    {/* image */}
-                    <div id='profileimage'>
-
-                    </div>
-                    {/* skills */}
-                    <div id='skills'>
-                            <h3>Skills</h3>
-                            <div id='skillicon'>
-                                <span id='js'><FaJs /></span>
-                                <span id='react'><FaReact /></span>
-                                <span id='py'><FaPython /></span>
-                                <span id='node'><FaNodeJs /></span>
-                            </div>
-                    </div>
+            <div className='maintitle'>
+                <h1>About Me</h1>
+            </div>
+            <div className='cv'>
+                <div className="someinfo">
+                    <p>
+                        Hey! <br />
+                        <span id='green'>I am Islam </span> <br />
+                        <span>FullStack Developer</span>
+                    </p>
                 </div>
-                {/* word about me */}
-                <div id='maincirtria'>
-                    <h1>About Me</h1>
-                    {/* content */}
-                    <div id='education'>
-                        <h3>Education</h3>
-                        <h5><span className='icon'>-</span>Tanta University</h5>
-                        <p className='word'>
-                            Study in The college of Science
-                            in Computer Science Department.
-                            in the First Year i was Learn C++ programmin Language
-                            Basics , in the Second Year learn data structure and algorithms and
-                            in the Third Year learn Software Engineering , System Analysis and
-                            Simulation.
-                        </p>
-                        <h5><span className='icon'>-</span>Self Learning</h5>
-                        <p className='word'>
-                            When I was Study in the university i
-                            was Planning to Learn the New Technologies
-                            depend on my self , where I Collect The 
-                            Resources From The Internet and learning 
-                            from this resources such as Youtube, W3 Schools
-                            and MDN ...
-                        </p>
-                    </div>
+                <div className="myphoto">
+                    <img src="/about.jpg" alt="photo" />
                 </div>
             </div>
+            <div id='breaker'></div>
             {/* Cirtifications */}
-                <h1 id='certificarion-head-text'>Cirtifications</h1>
-                <div id="cirt">
-                    <Swiper
-                    effect={'cards'}
-                    grabCursor={true}
-                    modules={[EffectCards]}
-                    className="mySwiper">
-                        {data.map((item) => {
-                            const {id, images} = item;
-                            return (
-                                    <SwiperSlide key={id}>
-                                        <img key={id} src={`${images}`} alt='certificate'/>
-                                    </SwiperSlide>
-                            )
-                        })}
-                    </Swiper>
-                </div>
-            {/* <Contact /> */}
+            <div id="cirt">
+                <Swiper
+                effect={'cards'}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className="mySwiper">
+                    {data.map((item) => {
+                        const {id, images} = item;
+                        return (
+                                <SwiperSlide key={id} className='photosInSwiper'>
+                                    <img key={id} src={`${images}`} alt='certificate'/>
+                                </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
+            </div>
         </section>
         <Footer />
     </>
